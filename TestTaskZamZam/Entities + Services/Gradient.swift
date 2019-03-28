41b39019a49996e107b6d3013326ce5715aa63a2
Atsamaz <atsabitsoev>
2019-03-28 14:38:50 +0300

    commit

--- TestTaskZamZam/Entities + Services/Gradient.swift
+++ TestTaskZamZam/Entities + Services/Gradient.swift
@@ -11,27 +11,18 @@ import UIKit
 extension UIView {
     
     func addGradient(colors: [CGColor], coordinatesX: [Int], coordinatesY:[Int], cornerRadius: CGFloat = 0) {
-        
         if self.layer.sublayers == nil {
             return
         }
         
         let gradient = CAGradientLayer()
-        
         gradient.frame = self.bounds
-        print("\(self.bounds) рамка")
-        
         gradient.colors = colors
-        
         gradient.startPoint = CGPoint(x: coordinatesX[0], y: coordinatesY[0])
         gradient.endPoint = CGPoint(x: coordinatesX[1], y: coordinatesY[1])
-        
         gradient.cornerRadius = cornerRadius
         
-        print(self.layer.sublayers!.count)
-        
         self.layer.insertSublayer(gradient, at: 0)
-        
     }
     
 }
