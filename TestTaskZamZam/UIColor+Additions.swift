29558c8146e8d09fafc502123c625c0195504967
Atsamaz <atsabitsoev>
2019-04-02 12:59:05 +0300

    Коммит

--- TestTaskZamZam/UIColor+Additions.swift
+++ TestTaskZamZam/UIColor+Additions.swift
@@ -1,39 +1,39 @@
-////
-////  UIColor+Additions.swift
-////  zam wallet 2.0
-////
-////  Generated on Zeplin. (15.03.2019).
-////  Copyright (c) 2019 __MyCompanyName__. All rights reserved.
-////
 //
-//import UIKit
+//  UIColor+Additions.swift
+//  zam wallet 2.0
 //
-//extension UIColor {
-//    
-//    @nonobjc class var veryLightPink65: UIColor {
-//        return UIColor(white: 222.0 / 255.0,
-//                       alpha: 0.65)
-//    }
-//    
-//    @nonobjc class var skyBlue: UIColor {
-//        return UIColor(red: 86.0 / 255.0,
-//                       green: 192.0 / 255.0,
-//                       blue: 253.0 / 255.0,
-//                       alpha: 1.0)
-//    }
-//    
-//    @nonobjc class var blush: UIColor {
-//        return UIColor(red: 242.0 / 255.0,
-//                       green: 134.0 / 255.0,
-//                       blue: 134.0 / 255.0,
-//                       alpha: 1.0)
-//    }
-//    
-//    @nonobjc class var blushPink: UIColor {
-//        return UIColor(red: 1.0,
-//                       green: 127.0 / 255.0,
-//                       blue: 131.0 / 255.0,
-//                       alpha: 1.0)
-//    }
-//    
-//}
+//  Generated on Zeplin. (15.03.2019).
+//  Copyright (c) 2019 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var veryLightPink65: UIColor {
+        return UIColor(white: 222.0 / 255.0,
+                       alpha: 0.65)
+    }
+    
+    @nonobjc class var skyBlue: UIColor {
+        return UIColor(red: 86.0 / 255.0,
+                       green: 192.0 / 255.0,
+                       blue: 253.0 / 255.0,
+                       alpha: 1.0)
+    }
+    
+    @nonobjc class var blush: UIColor {
+        return UIColor(red: 242.0 / 255.0,
+                       green: 134.0 / 255.0,
+                       blue: 134.0 / 255.0,
+                       alpha: 1.0)
+    }
+    
+    @nonobjc class var blushPink: UIColor {
+        return UIColor(red: 1.0,
+                       green: 127.0 / 255.0,
+                       blue: 131.0 / 255.0,
+                       alpha: 1.0)
+    }
+    
+}
