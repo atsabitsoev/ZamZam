6011b06ce1a7ebacaa20362bc0b2b328dc28eebb
Atsamaz <atsabitsoev>
2019-05-24 16:59:02 +0300

    commit

--- TestTaskZamZam/Main/MainVC.swift
+++ TestTaskZamZam/Main/MainVC.swift
@@ -177,7 +177,8 @@ class MainVC: UIViewController, UICollectionViewDelegate {
         activityIndicator.startAnimating()
         
         UIView.animate(withDuration: 0.3) {
-            self.stackViewBalance.center = CGPoint(x: self.stackViewBalanceStandardCenter!.x, y: self.stackViewBalanceStandardCenter!.y + 13)
+            self.stackViewBalance.center = CGPoint(x: self.stackViewBalanceStandardCenter!.x,
+                                                   y: self.stackViewBalanceStandardCenter!.y + 13)
         }
         
         fetchBills()
